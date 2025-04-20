3cb803d2d800baeb5a7981f2a50fd57ee53455f8
Honesty Beaton <h_beaton@mail.fhsu.edu>
2025-04-19 22:53:33 -0500

    first commit

---
+++ Week12/model/Student.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const studentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email:
+  {
+    type: String,
+    required: true,
+    unique: true
+  },
+  course:
+  {
+    type: String,
+    required: true
+  },
+  enrolledDate:
+  {
+    type: Date,
+    required: true
+  },
+});
+
+module.exports = mongoose.model("Student", studentSchema);
